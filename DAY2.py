15979b4afbfdfb01699c107b98675d07923d8bfe
Anku229 <57283125+Anku229@users.noreply.github.com>
2020-07-22 14:53:16 +0530

    Add files via upload

---
+++ DAY2.py
@@ -0,0 +1,395 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World")
+
+
+# In[4]:
+
+
+print("Hello      world")
+
+
+# In[5]:
+
+
+print("Python's World")
+
+
+# In[6]:
+
+
+print("python \t is great.")
+
+
+# In[7]:
+
+
+print("python \n is great.")
+
+
+# In[8]:
+
+
+print('Python\'s World')
+
+
+# In[9]:
+
+
+print("python \a is great.")
+
+
+# In[16]:
+
+
+name="Akanksha"
+age=23
+marks=99.78
+print("Name of person is",name,"age is",age,"marks is",marks)
+
+
+# In[20]:
+
+
+print("Name of person is %10s age is %10d marks is %10.2f"%(name,age,marks))
+
+
+# In[23]:
+
+
+print( f"Name of person is {name}  age is  {age}  marks is  {marks}")
+
+
+# In[27]:
+
+
+x=y=z=23
+
+
+# In[28]:
+
+
+id(x)
+
+
+# In[29]:
+
+
+id(y)
+
+
+# In[31]:
+
+
+id(z)
+
+
+# In[32]:
+
+
+z=12
+
+
+# In[33]:
+
+
+id(z)
+
+
+# In[34]:
+
+
+5**4
+
+
+# In[35]:
+
+
+23+23
+
+
+# In[36]:
+
+
+12-232
+
+
+# In[37]:
+
+
+12/31
+
+
+# In[38]:
+
+
+232/3
+
+
+# In[39]:
+
+
+23//2
+
+
+# In[40]:
+
+
+23/2
+
+
+# In[41]:
+
+
+24/5
+
+
+# In[42]:
+
+
+24//5
+
+
+# In[43]:
+
+
+23%3
+
+
+# In[44]:
+
+
+a=12
+b=23
+a==b
+
+
+# In[45]:
+
+
+c=12
+a==c
+
+
+# In[46]:
+
+
+c<b
+
+
+# In[47]:
+
+
+c>a
+
+
+# In[49]:
+
+
+c<=a
+
+
+# In[50]:
+
+
+c>=b
+
+
+# In[51]:
+
+
+a!=b
+
+
+# In[55]:
+
+
+a=234
+
+
+# In[56]:
+
+
+b=23
+
+
+# In[57]:
+
+
+a|b
+
+
+# In[58]:
+
+
+bin(a)
+
+
+# In[59]:
+
+
+bin(b)
+
+
+# In[60]:
+
+
+a & b
+
+
+# In[61]:
+
+
+a>12 or b>34
+
+
+# In[62]:
+
+
+a<23 and b>234
+
+
+# In[63]:
+
+
+a>12 and a>23
+
+
+# In[64]:
+
+
+m
+
+
+# In[66]:
+
+
+10>9 and 20>m
+
+
+# In[67]:
+
+
+10<9 and 20>m
+
+
+# In[69]:
+
+
+20>m  and 10<9
+
+str1="test 123"
+# In[70]:
+
+
+str1="test 123"
+
+
+# In[71]:
+
+
+"t" in str1
+
+
+# In[72]:
+
+
+a=12
+b=12
+
+
+# In[73]:
+
+
+a is b
+
+
+# In[74]:
+
+
+'p' not in str1
+
+
+# In[80]:
+
+
+x=1.2
+y=1.2
+x==y
+
+
+# In[81]:
+
+
+id(x)
+
+
+# In[82]:
+
+
+id(y)
+
+
+# In[83]:
+
+
+x is y
+
+
+# In[87]:
+
+
+a=343
+
+
+# In[89]:
+
+
+b=343
+
+
+# In[90]:
+
+
+id(a)
+
+
+# In[91]:
+
+
+id(b)
+
+
+# In[92]:
+
+
+12+23/23*2
+
+
+# In[93]:
+
+
+2**2**3
+
+
+# In[94]:
+
+
+4**3
+
+
+# In[95]:
+
+
+2**8
+
+
+# In[ ]:
+
+
+
+
