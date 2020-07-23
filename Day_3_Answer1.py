1eb57ecf66c102e377c42477935349455b772a9c
Anku229 <57283125+Anku229@users.noreply.github.com>
2020-07-23 19:19:41 +0530

    Add files via upload

---
+++ Day_3_Answer1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+a=input("Enter number to calculate sum: ")
+a=int(a)
+total_numbers=a
+sum=0
+while (a>=0):
+    sum=a+sum;
+    a-=1
+print("Result:",sum)
+
+
+# In[ ]:
+
+
+
+
